d6d4ef98a005f044363d6244448224d4c7854fee
glncmd <77870057+glncmd@users.noreply.github.com>
2021-10-09 10:12:58 +0800

    Update naming conventions, add description

--- ps-cmyk.py
+++ ps-cmyk.py
@@ -1,4 +1,5 @@
-# Converts a folder of RGB images to CMYK, 300dpi
+# Converts a folder of RGB images / psd files to CMYK, 300dpi
+
 # Used for preparing print-ready photos from a design project in bulk.
 
 from comtypes.client import GetActiveObject, CreateObject
@@ -8,27 +9,27 @@ from os.path import join
 app = GetActiveObject("Photoshop.Application", dynamic=True)
 
 # Folder paths
-directoryFolder = "C:\\Users\\Glen\\Desktop\\New folder"
-destinationFolder = "C:\\Users\\Glen\\Desktop\\New folder (2)"
+directory_folder = "C:\\Users\\Glen\\Desktop\\New folder"
+destination_folder = "C:\\Users\\Glen\\Desktop\\New folder (2)"
 
 # Color profiles
 RGB = "sRGB IEC61966-2.1"
 CMYK = "U.S. Web Coated (SWOP) v2"
 
 # Save options for jpeg
-jpegOptions = CreateObject("Photoshop.JPEGSaveOptions", dynamic=True)
-jpegOptions.EmbedColorProfile = True
-jpegOptions.FormatOptions = 1
-jpegOptions.Matte = 1
-jpegOptions.Quality = 12
+jpeg_options = CreateObject("Photoshop.JPEGSaveOptions", dynamic=True)
+jpeg_options.EmbedColorProfile = True
+jpeg_options.FormatOptions = 1
+jpeg_options.Matte = 1
+jpeg_options.Quality = 12
 
 # Save options for psd
-psdOptions = CreateObject("Photoshop.PhotoshopSaveOptions", dynamic=True)
-psdOptions.annotations = False
-psdOptions.alphaChannels = True
-psdOptions.layers = True
-psdOptions.spotColors = True
-psdOptions.embedColorProfile = True
+psd_options = CreateObject("Photoshop.PhotoshopSaveOptions", dynamic=True)
+psd_options.annotations = False
+psd_options.alphaChannels = True
+psd_options.layers = True
+psd_options.spotColors = True
+psd_options.embedColorProfile = True
 
 def resize(doc, resolution):
     # No resampling
@@ -37,13 +38,13 @@ def resize(doc, resolution):
 def convert(doc, profile):
     doc.ConvertProfile(profile, 3, True, True)
 
-for file in listdir(directoryFolder):
-    app.Open(join(directoryFolder, file))
+for file in listdir(directory_folder):
+    app.Open(join(directory_folder, file))
 
-    currentDoc = app.ActiveDocument
+    current_doc = app.ActiveDocument
     
-    resize(currentDoc, 300)
-    convert(currentDoc, CMYK)
-    
-    currentDoc.SaveAs(join(destinationFolder, file), psdOptions, False, None)
-    currentDoc.Close()
+    resize(current_doc, 300)
+    convert(current_doc, CMYK)
+
+    current_doc.SaveAs(join(destination_folder, file), psd_options, False, None)
+    current_doc.Close()
